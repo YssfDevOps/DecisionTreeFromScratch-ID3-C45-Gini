9f9918e204219afa295ffba7697e2e3317abc3c5
YssfDevOps <youssef.cah.7912@gmail.com>
2023-10-17 11:08:44 +0200

    main created

---
+++ main.py
@@ -0,0 +1,30 @@
+__authors__ = ['1638618, 1636517, 1633311']
+__group__ = 'GM08:30_3'
+
+import numpy as np
+from copy import copy, deepcopy
+import pandas as pnd
+
+def pruning():
+    pass
+
+def random_forest():
+    pass
+
+def fill_missing_values():
+    pass
+
+def leave_one_out():
+    pass
+
+def cross_validation():
+    pass
+
+def C45():
+    pass
+
+def ID3():
+    pass
+
+if __name__ == '__main__':
+    pass
