7d45534977095eeae00681ddfde925017fd03da7
jg03dev <jan.gra.ser.001@gmail.com>
2023-11-27 18:47:31 +0000

    DTC works but only in ID3

--- DecisionTreeClassifier.py
+++ DecisionTreeClassifier.py
@@ -1,227 +1,121 @@
 import numpy as np
-
+from collections import deque
 
 # Classe Node
 class Node:
-    def __init__(self, feature=None, left=None, right=None, value=None):
-        self.feature = feature
-        self.left = left
-        self.right = right
-        self.value = value
-
-    def is_leaf_node(self):
-        return self.value is not None
+    """Contains the information of the node and another nodes of the Decision Tree."""
+    def __init__(self):
+        self.value = None
+        self.next = None
+        self.childs = None
 
 
-class DecisionTreeCasifier:
-    def __init__(self, criterion='ID3'):
-        self.root = None
-        self.root_node = Node()
+class DecisionTreeClassifier:
+    def __init__(self, nombres_atributos, criterion='ID3'):
+        self.nodo = None
         self.criterion = criterion  # {'ID3_Entropy', 'C45_Entropy', 'ID3_Gini', 'C45_Gini'}
+        self.X = None
+        self.nombres_atributos = nombres_atributos
+        self.etiquetas = None
+        self.categoriasEtiquetas = None
+        self.conteo = None
+        self.entropia = None
 
     def __str__(self):  # Print tree
         if self.root_node.is_leaf_node():
             return f"There is no tree to be printed!"
         self.print_tree(self.root_node)
 
-    def calculo_entropia_total(self, X_train, clases, atributo):
-        entropia_total = 0
-        dim_fila = X_train.shape[0]
+    def calcular_entropy(self, lista_attr):
+        etiquetas = [self.etiquetas[i] for i in lista_attr]
+        conteo_etiquetas = [etiquetas.count(x) for x in self.categoriasEtiquetas]
+        entropia = sum([-conteo / len(lista_attr) * np.log2(conteo / len(lista_attr)) if conteo else 0 for conteo in
+                        conteo_etiquetas])
+        return entropia
 
-        for clase in clases:
-            num_total_clases = X_train[X_train[atributo] == clase].shape[0]
-            entropia_total_clase = - (num_total_clases / dim_fila) * np.log2(num_total_clases / dim_fila)
-            entropia_total += entropia_total_clase
+    def calcular_ganancia_informacion(self, lista_attr, id_caracteristica):
+        ganancia_info = self.calcular_entropy(lista_attr)
+        caracteristicas = [self.X[x][id_caracteristica] for x in lista_attr]
+        valuees_caracteristicas = list(set(caracteristicas))
+        conteo_valuees_caracteristicas = [caracteristicas.count(x) for x in valuees_caracteristicas]
+        attr_valuees_caracteristicas = [[lista_attr[i] for i, x in enumerate(caracteristicas) if x == y] for y in valuees_caracteristicas]
+        ganancia_info = ganancia_info - sum([conteo_valuees / len(lista_attr) * self.calcular_entropy(ids_valuees)
+                                             for conteo_valuees, ids_valuees in
+                                             zip(conteo_valuees_caracteristicas, attr_valuees_caracteristicas)])
+        return ganancia_info
 
-        return entropia_total
+    def obtener_maxima_ganancia_informacion(self, attr_muestras, attr_atributos):
+        entropia_atributos = [self.calcular_ganancia_informacion(attr_muestras, id_atributo) for id_atributo in attr_atributos]
+        id_maximo = attr_atributos[entropia_atributos.index(max(entropia_atributos))]
+        return self.nombres_atributos[id_maximo], id_maximo
 
-    def calculo_entropia_label(self, datos_atributo, clases, atributo):
-        entropia_total = 0
-        dim_fila = datos_atributo.shape[0]
+    def ID3(self):
+        lista_attr = [x for x in range(len(self.X))]
+        caracteristicas_attr = [x for x in range(len(self.nombres_atributos))]
+        self.nodo = self.ID3Recursive(lista_attr, caracteristicas_attr, self.nodo)
+        print('')
 
-        for clase in clases:
-            num_total_atributo = datos_atributo[datos_atributo[atributo] == clase].shape[0]
-            entropia_atributo = (- (num_total_atributo/dim_fila)*np.log2(num_total_atributo/dim_fila)) if num_total_atributo != 0 else 0
-            entropia_total += entropia_atributo
+    def ID3Recursive(self, lista_attr, caracteristicas_attr, nodo):
+        """ID3 algorithm. It is called recursively until some criteria is met.
+                Parameters
+                __________
+                :param x_ids: list, list containing the samples ID's
+                :param feature_ids: list, List containing the feature ID's
+                :param node: object, An instance of the class Nodes
+                __________
+                :returns: An instance of the class Node containing all the information of the nodes in the Decision Tree
+                """
+        if not nodo:
+            nodo = Node()  # initialize nodes
 
-        return entropia_total
+        attribute = [self.etiquetas[x] for x in lista_attr]
 
-    def gainID3(self, X_train, clases, atributo, caracteristica):
-        valor_info = 0
-        dim_fila = X_train.shape[0]
-        valors_caracteristica = X_train[caracteristica].unique()
+        # SPLIT CRITERION
+        if len(set(attribute)) == 1:
+            nodo.value = self.etiquetas[lista_attr[0]]
+            return nodo
+        if len(caracteristicas_attr) == 0:
+            nodo.value = max(set(attribute), key=attribute.count)  # compute mode
+            return nodo
 
-        for carac in valors_caracteristica:
-            carac_datos = X_train[X_train[caracteristica] == carac]
-            carac_dim = carac_datos.shape[0]
-            carac_probabilidad = carac_dim / dim_fila
-            carac_entropia = self.calculo_entropia_label(carac_datos, clases, atributo)
-            valor_info += carac_probabilidad * carac_entropia
+        best_feature_name, best_feature_id = self.obtener_maxima_ganancia_informacion(lista_attr, caracteristicas_attr)
+        nodo.value = best_feature_name
+        nodo.childs = []
+        # value of the chosen feature for each instance
+        feature_values = list(set([self.X[x][best_feature_id] for x in lista_attr]))
+        # loop through all the values
+        for value in feature_values:
+            child = Node()
+            child.value = value  # add a branch from the node to each feature value in our feature
+            nodo.childs.append(child)  # append new child node to current node
+            child_x_ids = [x for x in lista_attr if self.X[x][best_feature_id] == value]
+            if not child_x_ids:
+                child.next = max(set(attribute), key=attribute.count)
+                print('')
+            else:
+                if caracteristicas_attr and best_feature_id in caracteristicas_attr:
+                    to_remove = caracteristicas_attr.index(best_feature_id)
+                    caracteristicas_attr.pop(to_remove)
+                # recursively call the algorithm
+                child.next = self.ID3Recursive(child_x_ids, caracteristicas_attr, child.next)
+        return nodo
 
-        gain = self.calculo_entropia_total(X_train, clases, atributo) - valor_info
-        return gain
-
-    def ID3(self, X_train, clases, atributo):
-        gain_info = -1
-        caracteristica_info = None
-        caracteristicas = X_train.columns.drop(atributo)
-
-        for carac in caracteristicas:
-            gain = self.gainID3(X_train, clases, atributo, carac)
-            if gain_info < gain:
-                gain_info = gain
-                caracteristica_info = carac
-
-        return caracteristica_info
-
-    def calculo_ganancia_info(self, X_train, clases, atributo):
-        ganancia_info_total = 0
-        dim_fila = X_train.shape[0]
-
-        for clase in clases:
-            num_total_clases = X_train[X_train[atributo] == clase].shape[0]
-            if num_total_clases != 0:
-                ganancia_info_total_clase = - (num_total_clases / dim_fila) * np.log2(num_total_clases / dim_fila)
-                ganancia_info_total += ganancia_info_total_clase
-
-        return ganancia_info_total
-
-    def calculo_gini(self, datos_atributo, clases, atributo):
-        gini_total = 1
-        dim_fila = datos_atributo.shape[0]
-
-        for clase in clases:
-            num_total_atributo = datos_atributo[datos_atributo[atributo] == clase].shape[0]
-            prob = num_total_atributo / dim_fila
-            gini_total -= prob ** 2
-
-        return gini_total
-
-    def gain_ratio_guany(self, X_train, clases, atributo, caracteristica):
-        split_info = 0
-        dim_fila = X_train.shape[0]
-        valors_caracteristica = X_train[caracteristica].unique()
-
-        for carac in valors_caracteristica:
-            carac_datos = X_train[X_train[caracteristica] == carac]
-            carac_dim = carac_datos.shape[0]
-            carac_probabilidad = carac_dim / dim_fila
-            carac_ganancia_info = self.calculo_ganancia_info(carac_datos, clases, atributo)
-            if carac_probabilidad > 0:
-                split_info -= carac_probabilidad * np.log2(carac_probabilidad)
-
-        if split_info != 0:
-            gain_ratio = (self.calculo_ganancia_info(X_train, clases, atributo) - carac_ganancia_info) / split_info
-        else:
-            gain_ratio = 0
-
-        return gain_ratio
-
-    def gain_ratio_gini(self, X_train, clases, atributo, caracteristica):
-        split_info = 0
-        dim_fila = X_train.shape[0]
-        valors_caracteristica = X_train[caracteristica].unique()
-
-        for carac in valors_caracteristica:
-            carac_datos = X_train[X_train[caracteristica] == carac]
-            carac_dim = carac_datos.shape[0]
-            carac_probabilidad = carac_dim / dim_fila
-            carac_gini = self.calculo_gini(carac_datos, clases, atributo)
-            if carac_probabilidad > 0:
-                split_info -= carac_probabilidad * np.log2(carac_probabilidad)
-
-        if split_info != 0:
-            gain_ratio = (1 - self.calculo_gini(X_train, clases, atributo)) / split_info
-        else:
-            gain_ratio = 0
-
-        return gain_ratio
 
     def C45(self, X_train, clases, atributo, tipo=1):
-        gain_ratio_max = -1
-        caracteristica_max = None
-        caracteristicas = X_train.columns.drop(atributo)
+        pass
 
-        for carac in caracteristicas:
-            if tipo == 1:
-                gain_ratio = self.gain_ratio_guany(X_train, clases, atributo, carac)
-            else:  # asumimos que cualquier otro valor para 'tipo' debería usar 'gini'
-                gain_ratio = self.gain_ratio_gini(X_train, clases, atributo, carac)
-
-            if gain_ratio_max < gain_ratio:
-                gain_ratio_max = gain_ratio
-                caracteristica_max = carac
-
-        return caracteristica_max
-
-    def SplitCriterion(self, X_train, clases, atributo):  # {'ID3_Entropy', 'C45_Entropy', 'C45_Gini'}
-        if self.criterion == 'ID3_Gini':  # ID 3 Gini
-            return self.ID3(X_train, clases, atributo)
-        elif self.criterion == 'C45_Entropy':  # C4.5 Entropy
+    def SplitCriterion(self, X_train, clases, atributo):  # {'ID3', 'C45_Entropy', 'C45_Guany'}
+        if self.criterion == 'C45_Guany':  # C4.5 Entropy
             return self.C45(X_train, clases, atributo, 1)
         elif self.criterion == 'C45_Gini':  # C4.5 Gini
             return self.C45(X_train, clases, atributo, 0)
         else:  # ID3_Entropy by default
             return self.ID3(X_train, clases, atributo)  # ID 3 Entropy
 
-    def StoppingCriterion(self, X_train):
-        # Cuando todos los ejemplos que quedan pertenecen a la misma clase.
-        if np.unique(X_train).size == 1:
-            return True
-
-        # Cuando no quedan atributos por los que ramificar.
-        if X_train.shape[1] == 0:
-            return True
-
-        # Cuando no quedan datos para clasificar.
-        if X_train.shape[0] == 0:
-            return True
-
-        return False
-
-    def tree_growing(self, X_train, objectiu, clases, tipus):
-        self.criterion = tipus
-        if self.StoppingCriterion(X_train) is False:
-            caracteristica = self.SplitCriterion(X_train, clases, objectiu)
-
-            # Aquí es donde se genera el subárbol
-            feature_value_count_dict = X_train[caracteristica].value_counts(sort=False)
-            tree = {}
-
-            for feature_value, count in feature_value_count_dict.iteritems():
-                feature_value_data = X_train[X_train[caracteristica] == feature_value]
-
-                assigned_to_node = False
-                for c in clases:
-                    class_count = feature_value_data[feature_value_data[objectiu] == c].shape[0]
-
-                    if class_count == count:
-                        tree[feature_value] = Node(value=c)
-                        X_train = X_train[X_train[caracteristica] != feature_value]
-                        assigned_to_node = True
-                if not assigned_to_node:
-                    tree[feature_value] = Node(value="?")
-
-            next_root = None
-
-            if self.root != None:
-                self.root.feature[caracteristica] = dict()
-                self.root.feature[caracteristica][caracteristica] = tree
-                next_root = self.root.feature[caracteristica][caracteristica]
-            else:
-                self.root = Node(feature=caracteristica)
-                self.root.feature[caracteristica] = tree
-                next_root = self.root.feature[caracteristica]
-
-            for node, branch in list(next_root.items()):
-                if branch.value == "?":
-                    feature_value_data = X_train[X_train[caracteristica] == node]
-                    self.tree_growing(feature_value_data, objectiu, clases, tipus)
-
     def predict_rec(self, X, node):
         if node.is_leaf_node():
             return node.value
-        elif X[node.feature] <= node.threshold:
+        elif X[node.feature] <= node.value:
             return self.predict_rec(X, node.left)
         else:
             return self.predict_rec(X, node.right)
@@ -229,20 +123,31 @@ class DecisionTreeCasifier:
     def predict(self, X):
         return self.predict_rec(X, self.root_node)
 
-    def fit(self, X_train, y_train):
-        if len(X_train) != len(y_train):
+    def fit(self, X, y):
+        if len(X) != len(y):
             raise ValueError("X_train and y_train must have the same number of samples")
+        # Inicializar variables en base a los datos
+        self.X = X
+        self.etiquetas = y
+        self.categoriasEtiquetas = list(set(y))
+        self.conteo = [list(y).count(x) for x in self.categoriasEtiquetas]
+        self.entropia = self.calcular_entropy([x for x in range(len(self.etiquetas))])  # calculates the initial entropy
 
-            # Use NumPy's concatenate function to combine the data along the appropriate axis
-        S = np.concatenate((X_train, y_train.reshape(-1, 1)), axis=1)
-        return self.tree_growing(S, X_train, y_train)
+        self.ID3() # Aqui va lo de decidir el criterio
 
 
-    def print_tree(self, node, depth=0):
-        if node.is_leaf_node():
-            print(f"{depth * '  '}[{node.value}]")
+    def print_tree(self):
+        if not self.nodo:
             return
+        nodes = deque()
+        nodes.append(self.nodo)
+        while len(nodes) > 0:
+            node = nodes.popleft()
+            print(node.value)
+            if node.childs:
+                for child in node.childs:
+                    print('({})'.format(child.value))
+                    nodes.append(child.next)
+            elif node.next:
+                print(node.next)
 
-        print(f"{depth * '  '}[{node.feature} <= {node.threshold}]")
-        self.print_tree(node.left, depth + 1)
-        self.print_tree(node.right, depth + 1)
